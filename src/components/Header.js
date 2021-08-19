c2c6c7e3a2243f11591daefc6aed30fc4620fce1
burcu-erkan <burcuerkan.86@gmail.com>
2021-08-19 12:02:20 -0700

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,9 @@ import React from 'react';
 function Header() {
     return (
         <header>
+             <h1>React Weather App</h1>
             <img src="https://nordicapis.com/wp-content/uploads/5-Best-Free-and-Paid-Weather-APIs-2019-e1587582023501.png" alt="weather logo"/>
-                <h1>React Weather App</h1>
+               
         </header>
     )
 }
